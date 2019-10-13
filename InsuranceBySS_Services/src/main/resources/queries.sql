77e69e45492dc01f437dd44d5b8ebf05d3a9694a
Narsingh <narsinghraom.dhatriinfo@gmail.com>
2019-10-13 17:42:02 +0530

    queries

---
+++ InsuranceBySS_Services/src/main/resources/queries.sql
@@ -0,0 +1,4 @@
+create table CUSTOMER_TABLE (customerReferanceId varchar(50), customerFirstName varchar(30), customerLastName varchar(30), customerAddress varchar(60), customerDOB varchar(20));
+
+insert into CUSTOMER_TABLE values("A100","Narsingh","Mahankali","Hyderabad","Jan-12");
+insert into CUSTOMER_TABLE values("A200","Mounika","Mahankali","Hyderabad","Aug-08");
